277db8f672bd45ad1f677aca5b23fb6994234b78
joaquin <joaquinmaciel2910@gmail.com>
2026-02-12 21:51:36 +0100

    all components, dashboard and index added

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,10 +1,11 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.tsx'
+import { App } from './App';
+import DashboardPage from './pages/DashboardPage';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <DashboardPage />
   </StrictMode>,
 )
